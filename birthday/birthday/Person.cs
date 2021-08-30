81bc6bd3f8d8c8b4f39ecc04ad10bb190565026f
David O Mahony <davidmahony@microsoft.com>
2021-08-30 17:24:23 +0100

    Adding person and person builder

---
+++ birthday/birthday/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birthday
+{
+    public class Person
+    {
+        public DateTime Birthday { get; set; }
+
+        // Leaving this as a list as it is common for someone to have more then two names
+        public List<string> Names { get; set; } = new List<string>();
+    }
+}
---
+++ birthday/birthday/PersonBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Birthday
+{
+    public class PersonBuilder
+    {
+        private string[] inputs;
+
+        public void SetPersonInputs(string[] inputs)
+        {
+            this.inputs = inputs;
+        }
+
+        /// <summary>
+        /// As the input is a string array we dont know how many names a person could have
+        /// SO a valid person will have a list of names (which should not contain letters) and only one date.
+        /// </summary>
+        /// <param name="outputtedperson"></param>
+        /// <returns></returns>
+        public bool TryBuild(out Person outputtedperson)
+        {
+            outputtedperson = null;
+            return false;
+        }
+    }
+}
